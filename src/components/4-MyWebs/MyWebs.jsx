0019d7a132e243fb3dc4ffe0d6e2422fc7d9612a
Franandrade94 <franandrade15994@gmail.com>
2023-03-18 05:17:46 -0300

    Projects finish

--- src/components/4-MyWebs/MyWebs.jsx
+++ src/components/4-MyWebs/MyWebs.jsx
@@ -4,35 +4,54 @@ import Dogs from "./Webs-Card/Dogs";
 import Bonita from "./Webs-Card/BonitaDevoto";
 import Pokemon from "./Webs-Card/PokemonCard";
 import Videogames from "./Webs-Card/VideoGames";
-
-
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 class MyWebs extends Component {
-    render() {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 790,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+          },
+        },
+      ],
+    };
 
-      return (
-        <div className="Project-All" id="projects">
-          <div className="Project-Container">
-            <h2 className="Project-Title">My Projects</h2>
-            
-                <div>
-                    <Bonita />
-                </div>
-                <div>
-                    <Dogs />
-                </div>
-                <div>
-                    <Videogames />
-                </div>
-                <div>
-                    <Pokemon />
-                </div>
-              
+    return (
+      <div className="Project-All" id="projects">
+        <div className="Project-Container ">
+          <h2 className="Project-Title">My Projects</h2>
+          <Slider {...settings}>
+            <div>
+              <Bonita />
             </div>
+            <div>
+              <Dogs />
+            </div>
+            <div>
+              <Videogames />
+            </div>
+            <div>
+              <Pokemon />
+            </div>
+          </Slider>
         </div>
-      );
-    }
+      </div>
+    );
   }
-  
-  export default MyWebs;
+}
+
+export default MyWebs;
