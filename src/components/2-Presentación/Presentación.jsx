5a510e165ba45d1d82a1b1471fdf94f2fd670543
Franandrade94 <franandrade15994@gmail.com>
2023-03-20 15:20:57 -0300

    WhatsApp Link

--- src/components/2-Presentación/Presentación.jsx
+++ src/components/2-Presentación/Presentación.jsx
@@ -8,7 +8,7 @@ const Presentacion = () => {
 
     
     const handleClickWhatsapp = () => {
-        window.open("")
+        window.open("https://wa.me/5491156073772?text=Hola,%20Francisco%20¿Cómo%20estas?%20Vi%20tu%20página%20personal%20y%20me%20gustaría%20contactarte.%20Muchas%20Gracias!%20")
     }
 
     const handleClickGitHub = () => {
